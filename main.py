956ff8c5519f9f39c7adb27c8ec47857185deaad
DemanP <72989641+DemanP@users.noreply.github.com>
2023-02-24 07:15:14 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -84,85 +84,129 @@
 
 
 
-import logging
-import time
+# import logging
+# import time
+# from translate import *
+
+# import flask
+
+# import telebot
+
+# API_TOKEN = '6072135388:AAE2YOduxp_PVixfwzRdCeBvFW1R7dLmJ0c'
+
+# WEBHOOK_HOST = 'oluhenia-translate.onrender.com'
+# WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+# WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+
+# WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+# WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+
+# # Quick'n'dirty SSL certificate generation:
+# #
+# # openssl genrsa -out webhook_pkey.pem 2048
+# # openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
+# #
+# # When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
+# # with the same value in you put in WEBHOOK_HOST
+
+# WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+# WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
+
+# logger = telebot.logger
+# telebot.logger.setLevel(logging.INFO)
+
+# bot = telebot.TeleBot(API_TOKEN)
+
+# app = flask.Flask(__name__)
+
+
+# # Empty webserver index, return nothing, just http 200
+# @app.route('/', methods=['GET', 'HEAD'])
+# def index():
+#     return ''
+
+
+# # Process webhook calls
+# @app.route(WEBHOOK_URL_PATH, methods=['POST'])
+# def webhook():
+#     if flask.request.headers.get('content-type') == 'application/json':
+#         json_string = flask.request.get_data().decode('utf-8')
+#         update = telebot.types.Update.de_json(json_string)
+#         bot.process_new_updates([update])
+#         return ''
+#     else:
+#         flask.abort(403)
+
+
+# # Handle '/start' and '/help'
+# @bot.message_handler(commands=['help', 'start'])
+# def send_welcome(message):
+#     bot.reply_to(message, f"Hau {message.from_user.first_name} {message.from_user.last_name}!\nНапиши мені речення на українській і я його перекладу на олутонк(або навпаки, з олутонку на українську)")
+
+
+# # Handle all other messages
+# @bot.message_handler(func=lambda message: True, content_types=['text'])
+# def echo_message(message):
+#     result = translate(message.text)
+#     bot.reply_to(message, result)
+
+
+# # Remove webhook, it fails sometimes the set if there is a previous webhook
+# bot.remove_webhook()
+
+# time.sleep(1)
+
+# # Set webhook
+# bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+#                 # certificate=open(WEBHOOK_SSL_CERT, 'r')
+# )
+
+# # Start flask server
+# app.run(host=WEBHOOK_LISTEN,
+#         port=WEBHOOK_PORT,
+#         # ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
+#         debug=True)
+
+
+
+from telegram.ext.updater import Updater
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.callbackqueryhandler import CallbackQueryHandler
+from telegram.ext.filters import Filters
+from telegram import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
+
+import os
 from translate import *
 
-import flask
+TOKEN = "6072135388:AAE2YOduxp_PVixfwzRdCeBvFW1R7dLmJ0c"
+PORT = int(os.environ.get('PORT', '8443'))
+updater = Updater(TOKEN)
 
-import telebot
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text(f"Hau {message.from_user.first_name} {message.from_user.last_name}!\nНапиши мені речення на українській і я його перекладу на олутонк(або навпаки, з олутонку на українську)")
 
-API_TOKEN = '6072135388:AAE2YOduxp_PVixfwzRdCeBvFW1R7dLmJ0c'
+def message_reply(update: Update, context: CallbackContext):
+    print(update.message)
+    update.message.reply_text(translate(update.message.text))
 
-WEBHOOK_HOST = 'oluhenia-translate.onrender.com'
-WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
-WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+def callback(update: Update, context):
+    data = update.callback_query.data
 
-WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
-WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+updater.dispatcher.add_handler(CommandHandler('start', start, pass_job_queue=True))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, message_reply))
+updater.dispatcher.add_handler(CallbackQueryHandler(callback))
 
-# Quick'n'dirty SSL certificate generation:
-#
-# openssl genrsa -out webhook_pkey.pem 2048
-# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
-#
-# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
-# with the same value in you put in WEBHOOK_HOST
+def callback_second(context: CallbackContext):
+    # print("10 seconds")
+    a = 0
+j = updater.job_queue
+job_minute = j.run_repeating(callback_second, interval=10, first=1)
 
-WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
-
-logger = telebot.logger
-telebot.logger.setLevel(logging.INFO)
-
-bot = telebot.TeleBot(API_TOKEN)
-
-app = flask.Flask(__name__)
-
-
-# Empty webserver index, return nothing, just http 200
-@app.route('/', methods=['GET', 'HEAD'])
-def index():
-    return ''
-
-
-# Process webhook calls
-@app.route(WEBHOOK_URL_PATH, methods=['POST'])
-def webhook():
-    if flask.request.headers.get('content-type') == 'application/json':
-        json_string = flask.request.get_data().decode('utf-8')
-        update = telebot.types.Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return ''
-    else:
-        flask.abort(403)
-
-
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message, f"Hau {message.from_user.first_name} {message.from_user.last_name}!\nНапиши мені речення на українській і я його перекладу на олутонк(або навпаки, з олутонку на українську)")
-
-
-# Handle all other messages
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    result = translate(message.text)
-    bot.reply_to(message, result)
-
-
-# Remove webhook, it fails sometimes the set if there is a previous webhook
-bot.remove_webhook()
-
-time.sleep(1)
-
-# Set webhook
-bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
-                # certificate=open(WEBHOOK_SSL_CERT, 'r')
-)
-
-# Start flask server
-app.run(host=WEBHOOK_LISTEN,
-        port=WEBHOOK_PORT,
-        # ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
-        debug=True)
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN,
+                      webhook_url = "https://oluhenia-translate.onrender.com/" + TOKEN)
+updater.idle()
